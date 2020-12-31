58cef5f170abc5ffe2cfaa8dd02d82db377ac738
cubiss99 <cubiss0203@gmail.com>
2020-12-31 17:32:36 +0700

    fist commit

---
+++ Command/index11.php
@@ -0,0 +1,93 @@
+<?php
+
+class Bulb
+{
+    public function turnOn()
+    {
+        echo "Bulb has been lit";
+    }
+
+    public function turnOff()
+    {
+        echo "Darkness!";
+    }
+}
+// then we have an interface that each of the commands are going to implement and then we have a set of commands
+
+interface Command
+{
+    public function execute();
+    public function undo();
+    public function redo();
+}
+
+// Command
+class TurnOn implements Command
+{
+    protected $bulb;
+
+    public function __construct(Bulb $bulb)
+    {
+        $this->bulb = $bulb;
+    }
+
+    public function execute()
+    {
+        $this->bulb->turnOn();
+    }
+
+    public function undo()
+    {
+        $this->bulb->turnOff();
+    }
+
+    public function redo()
+    {
+        $this->execute();
+    }
+}
+
+class TurnOff implements Command
+{
+    protected $bulb;
+
+    public function __construct(Bulb $bulb)
+    {
+        $this->bulb = $bulb;
+    }
+
+    public function execute()
+    {
+        $this->bulb->turnOff();
+    }
+
+    public function undo()
+    {
+        $this->bulb->turnOn();
+    }
+
+    public function redo()
+    {
+        $this->execute();
+    }
+}
+// Then we have an Invoker with whom the client will interact to process any commands
+
+// Invoker
+class RemoteControl
+{
+    public function submit(Command $command)
+    {
+        $command->execute();
+    }
+}
+// Finally let's see how we can use it in our client
+
+$bulb = new Bulb();
+
+$turnOn = new TurnOn($bulb);
+$turnOff = new TurnOff($bulb);
+
+$remote = new RemoteControl();
+$remote->submit($turnOn); // Bulb has been lit!
+$remote->submit($turnOff); // Darkness!
---
+++ State/index16.php
@@ -0,0 +1,72 @@
+<?php
+interface WritingState
+{
+    public function write(string $words);
+}
+
+class UpperCase implements WritingState
+{
+    public function write(string $words)
+    {
+        echo strtoupper($words);
+    }
+}
+
+class LowerCase implements WritingState
+{
+    public function write(string $words)
+    {
+        echo strtolower($words);
+    }
+}
+
+class DefaultText implements WritingState
+{
+    public function write(string $words)
+    {
+        echo $words;
+    }
+}
+// Then we have our editor
+
+class TextEditor
+{
+    protected $state;
+
+    public function __construct(WritingState $state)
+    {
+        $this->state = $state;
+    }
+
+    public function setState(WritingState $state)
+    {
+        $this->state = $state;
+    }
+
+    public function type(string $words)
+    {
+        $this->state->write($words);
+    }
+}
+// And then it can be used as
+
+$editor = new TextEditor(new DefaultText());
+
+$editor->type('First line');
+
+$editor->setState(new UpperCase());
+
+$editor->type('Second line');
+$editor->type('Third line');
+
+$editor->setState(new LowerCase());
+
+$editor->type('Fourth line');
+$editor->type('Fifth line');
+
+// Output:
+// First line
+// SECOND LINE
+// THIRD LINE
+// fourth line
+// fifth line
